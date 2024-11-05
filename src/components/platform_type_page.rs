14a1874a9d6904de7bd59898a86095515acfa2b1
canarit <gaqu1994@gmail.com>
2024-11-05 12:41:01 +0100

    fix route navigation add ico

--- src/components/platform_type_page.rs
+++ src/components/platform_type_page.rs
@@ -1,24 +1,39 @@
 use crate::components::summoner_search_page::SummonerSearchPage;
-use leptos::prelude::{expect_context, signal, ClassAttribute, Set};
+use leptos::prelude::{expect_context, signal, use_context, ClassAttribute, Get, Set, Show};
 use leptos::prelude::ElementChild;
 use leptos::{component, view, IntoView};
 use leptos::context::provide_context;
 use leptos_meta::{provide_meta_context, Link, Meta, Title};
 use leptos_router::components::Outlet;
+use leptos_router::location::RequestUrl;
 use crate::app::{MetaStore, MetaStoreStoreFields};
 
 #[component]
 pub fn PlatformTypePage() -> impl IntoView {
     let meta_store = expect_context::<reactive_stores::Store<MetaStore>>();
+
     meta_store.title().set("Broken.gg | High-Performance League of Legends Stats and Profiles".to_string());
     meta_store.description().set("Experience lightning-fast League of Legends statistics and summoner profiles on Broken.gg. Built with Rust for unmatched performance and efficiency. Search now to elevate your gaming experience.".to_string());
     meta_store.image().set("/favicon.ico".to_string());
     meta_store.url().set("".to_string());
+
+    let req_include_summoner = || {
+        let context = use_context::<leptos_router::components::RouterContext>();
+        if let Some(router_context) = context {
+            router_context.current_url.read_only().get().path().contains("summoners")
+        } else {
+            false
+        }
+    };
+
     view! {
         <div class="my-0 mx-auto max-w-3xl text-center">
             <a href="/" class="p-6 text-4xl my-4">
                 "Welcome to Broken.gg"
             </a>
+            <Show when= move || !req_include_summoner()>
+                <img src="/logo.webp" class="w-[420px] h-[420px] mx-auto" />
+            </Show>
             <SummonerSearchPage />
             <Outlet />
         </div>
--- src/components/summoner_encounters_page.rs
+++ src/components/summoner_encounters_page.rs
@@ -3,6 +3,7 @@ use leptos::prelude::{expect_context, ReadSignal, Set};
 use leptos_meta::{provide_meta_context, Meta, Title};
 use crate::models::entities::summoner::Summoner;
 use leptos::prelude::ElementChild;
+use leptos_router::components::A;
 use crate::app::{MetaStore, MetaStoreStoreFields};
 
 #[component]
@@ -13,5 +14,5 @@ pub fn SummonerEncountersPage() -> impl IntoView {
     meta_store.title().set(format!("{}#{} | Encounters | Broken.gg", summoner().game_name, summoner().tag_line));
     meta_store.description().set(format!("Discover the top champions played by {}#{}. Access in-depth statistics, win rates, and performance insights on Broken.gg, powered by Rust for optimal performance.", summoner().game_name, summoner().tag_line));
     meta_store.url().set(format!("{}?tab=encounters",summoner().to_route_path()));
-    view! { <h2>"Summoner Encounters Page"</h2> }
+    view! {<h2>"Summoner Encounters Page"</h2> }
 }
