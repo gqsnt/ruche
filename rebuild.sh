3bcfb638b7059aa84dd75c850c9ceaa6c4d46bdc
canarit <gaqu1994@gmail.com>
2024-11-11 10:06:46 +0100

    update rebuild.sh for no down time

--- rebuild.sh
+++ rebuild.sh
@@ -2,8 +2,9 @@
 git pull;
 cargo leptos build --release;
 systemctl stop leptos_broken_gg.service;
-mkdir -p /etc/leptos-broken-gg-release/target;
-cp /etc/leptos-broken-gg/target/release/leptos-broken-gg /etc/leptos-broken-gg-release/target/release/leptos-broken-gg;
-cp /etc/leptos-broken-gg/target/site /etc/leptos-broken-gg-release/target/site;
+mkdir -p /etc/leptos-broken-gg-release/target/release;
+mkdir -p /etc/leptos-broken-gg-release/target/site;
+mv /etc/leptos-broken-gg/target/release/leptos-broken-gg /etc/leptos-broken-gg-release/target/release/leptos-broken-gg;
+mv /etc/leptos-broken-gg/target/site/* /etc/leptos-broken-gg-release/target/site/*;
 systemctl start leptos_broken_gg.service;
 journalctl --follow -u leptos_broken_gg.service;
