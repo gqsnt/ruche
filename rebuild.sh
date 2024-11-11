56f9d61deb18ad567b54089c990ba4b716db649a
canarit <gaqu1994@gmail.com>
2024-11-11 09:44:05 +0100

    update rebuild.sh

--- rebuild.sh
+++ rebuild.sh
@@ -3,4 +3,4 @@ systemctl stop leptos_broken_gg.service;
 git pull;
 cargo leptos build --release;
 systemctl start leptos_broken_gg.service;
-systemctl status leptos_broken_gg.service;
+journalctl --follow -u leptos_broken_gg.service;
