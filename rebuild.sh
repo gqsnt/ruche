4e6b9a688fed28113060199f871c8e45aae84c75
canarit <gaqu1994@gmail.com>
2024-11-13 08:41:47 +0100

    update rebuild for full rust

--- rebuild.sh
+++ rebuild.sh
@@ -5,8 +5,10 @@ systemctl stop leptos_broken_gg.service;
 rm -rf /etc/leptos-broken-gg-release; ## RM -RF USED !!! Remove the old release
 mkdir -p /etc/leptos-broken-gg-release/target/release;
 mkdir -p /etc/leptos-broken-gg-release/target/site;
+mkdir -p /etc/leptos-broken-gg-release/signed_certs;
 cp -nf /etc/leptos-broken-gg/target/release/leptos-broken-gg /etc/leptos-broken-gg-release/target/release/leptos-broken-gg;
 cp -nfR /etc/leptos-broken-gg/target/site/* /etc/leptos-broken-gg-release/target/site/;
 cp -nf /etc/leptos-broken-gg/.env /etc/leptos-broken-gg-release/.env;
+cp -nf /etc/leptos-broken-gg/signed_certs/* /etc/leptos-broken-gg-release/signed_certs/;
 systemctl start leptos_broken_gg.service;
 journalctl --follow -u leptos_broken_gg.service;
