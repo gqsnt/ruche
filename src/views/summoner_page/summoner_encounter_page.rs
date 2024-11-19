5507399c9400c76f1bdd648488775c101b0eca12
canarit <gaqu1994@gmail.com>
2024-11-19 14:20:05 +0100

    fix winrate

--- src/views/summoner_page/summoner_encounter_page.rs
+++ src/views/summoner_page/summoner_encounter_page.rs
@@ -452,10 +452,7 @@ pub fn SummonerEncounterStat(summoner: Summoner, stats: SummonerEncounterStats, 
             >
                 <div>
                     {stats.total_wins}W {stats.total_loses}L {stats.total_wins + stats.total_loses}G
-                    {((
-                        stats.total_wins as f64
-                            / (stats.total_wins + stats.total_loses).max(1) as f64
-                    ) * 100.0).round() as i32}%
+                    {format!("{}%",((stats.total_wins as f64/ (stats.total_wins + stats.total_loses).max(1) as f64) * 100.0).round() as i32)}
                 </div>
                 <div class="flex flex-col">
                     <div>{stats.avg_kills}/ {stats.avg_deaths}/ {stats.avg_assists}</div>
